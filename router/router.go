4a6a758ec33b86c9faebb234cc612e353be8fad9
a3magic3pocket <a3magic3pocket3@gmail.com>
2021-10-28 12:09:10 +0900

    Make router.go

---
+++ router/router.go
@@ -0,0 +1,49 @@
+package router
+
+import (
+	"log"
+	"net/http"
+	"simple-api-example/controllers"
+	"simple-api-example/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRouter : 라우터 세팅
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	authMiddleware, err := middleware.GetAuthMiddleware()
+	if err != nil {
+		log.Fatal("JWT Error: " + err.Error())
+	}
+
+	// 인증
+	router.POST("/login", authMiddleware.LoginHandler)
+	router.POST("/logout", authMiddleware.LogoutHandler)
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Page Not Found"})
+	})
+
+	// 유저
+	user := router.Group("/user")
+	user.POST("", controllers.CreateUser)
+
+	// 로커
+	lockers := router.Group("/lockers")
+	lockers.Use(authMiddleware.MiddlewareFunc())
+	{
+		lockers.GET("", controllers.RetreiveLockers)
+		lockers.GET("all", controllers.RetreiveAllLocker)
+		lockers.POST("", controllers.CreateLockers)
+	}
+
+	locker := router.Group("/locker")
+	locker.Use(authMiddleware.MiddlewareFunc())
+	{
+		locker.GET(":id", controllers.RetreiveLocker)
+		locker.PATCH("", controllers.UpdateLocker)
+	}
+
+	return router
+}

242bc2fa163cbbd66cdfb65ae8f86420cb9f8331
a3magic3pocket <a3magic3pocket3@gmail.com>
2021-10-28 12:11:00 +0900

    Make logger.go

---
+++ logger/logger.go
@@ -0,0 +1,14 @@
+package logger
+
+import (
+	"runtime"
+)
+
+func GetPrefix() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+
+	return frame.Function
+}
